1b7b3116f4f87edc6098099862942501aa7d23c1
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-02-16 20:05:40 +0100

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ExpressoBtn from './ExpressoBtn/ExpressoBtn';
 export const App = () => {
   return (
     <div
@@ -10,6 +10,8 @@ export const App = () => {
         fontSize: 40,
         color: '#010101',
       }}
-    ></div>
+    >
+      <ExpressoBtn />
+    </div>
   );
 };
---
+++ src/components/ExpressoBtn/ExpressoBtn.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class ExpressoBtn extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLiveFeadback = value => {
+    this.setState({ [value]: this.state[value] + 1 });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    return (this.countTotalFeedback() * this.state.good) / 100;
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Please leave your featbeak</h1>
+        <button onClick={() => this.onLiveFeadback('good')}>Good</button>
+        <button onClick={() => this.onLiveFeadback('neutral')}>Neutral</button>
+        <button onClick={() => this.onLiveFeadback('bad')}>Bad</button>
+
+        <h2>Statistics</h2>
+        <ul>
+          <li>
+            <span>good</span>
+            <span>{this.state.good}</span>
+          </li>
+          <li>
+            <span>Neutral</span>
+            <span>{this.state.neutral}</span>
+          </li>
+          <li>
+            <span>Bad</span>
+            <span>{this.state.bad}</span>
+          </li>
+          <li>
+            <span>Total</span>
+            <span>{this.countTotalFeedback()}</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ExpressoBtn;
