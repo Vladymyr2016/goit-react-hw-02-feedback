3cd4fdb2d8d804b473e2d9a3279a8935e22f0f91
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-02-16 15:42:43 +0100

    z

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,4 @@
 import React from 'react';
-import Profile from '../components/Profile/Profile.jsx';
-import user from './Profile/user.json';
-import Statistics from './Statistics/Statistics.jsx';
-import data from './Statistics/data.json';
-import friendInfo from './FriendList/Friends.json';
-import FriendList from './FriendList/FriendList.jsx';
-import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
-import payments from './TransactionHistory/Transaction.json';
 
 export const App = () => {
   return (
@@ -18,14 +10,6 @@ export const App = () => {
         fontSize: 40,
         color: '#010101',
       }}
-    >
-      <Profile user={user} />
-
-      <Statistics data={data} />
-
-      <FriendList Friends={friendInfo} />
-
-      <TransactionHistory manyInfo={payments} />
-    </div>
+    ></div>
   );
 };
